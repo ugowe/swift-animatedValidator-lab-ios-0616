e47c68f6cefa936afe81dfd41d68ed54fe316398
susanlovaglio <susan@flatironschool.com>
2016-06-30 10:33:31 -0400

    removed extra xctests

--- animated-validator-swiftUITests/animated_validator_swiftUITests.swift
+++ animated-validator-swiftUITests/animated_validator_swiftUITests.swift
@@ -28,9 +28,6 @@ class animated_validator_swiftUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    
     
 }
